0785970b0f3f22ca8cb3bc4ea1319061e90b4c16
Gleb Kuznetsov <64061324+Zea-Zee@users.noreply.github.com>
2024-07-30 14:13:03 +0800

    some handle manage, it will be deleted

--- app.py
+++ app.py
@@ -12,13 +12,16 @@ def main():
     # database.create_tables()
     # database.add_skin_package("Global Warfare", 31, datetime.strptime("07/27/24 02:00:00", "%m/%d/%y %H:%M:%S"), datetime.strptime("08/03/24 07:00:00", "%m/%d/%y %H:%M:%S"))
 
+    # database.add_skin("ROCK", None, 1)
+    # database.add_skin("HAMMER", None, 1)
+    # database.add_skin("ARMORED DOUBLE DOOR", None, 1)
+    # database.add_skin("LARGE WOOD BOX", None, 1)
+    # database.add_skin("GARAGE DOOR", None, 1)
+    # database.add_skin("M249", None, 1)
 
-    database.add_skin("ROCK", None, 1)
-    database.add_skin("HAMMER", None, 1)
-    database.add_skin("ARMORED DOUBLE DOOR", None, 1)
-    database.add_skin("LARGE WOOD BOX", None, 1)
-    database.add_skin("GARAGE DOOR", None, 1)
-    database.add_skin("M249", None, 1)
+    # user_generator.create_users_in_database(1)
+    # database.update_email_status(["thed.arknessprince1997@gmail.com", "the.darknessprince1997@gmail.com"])
+    # database.reset_database(drop_users=True)
 
 
 if __name__ == '__main__':
